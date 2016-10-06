f82c0e6cb203615cff38e53c23de6ad2c722094b
pavan_08 <pavanchhatpar@gmail.com>
2016-10-06 19:20:03 +0530

    Initializing certain pages and setting up font-awesome in style.min.css

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>VSM WT</title>
+		<link rel="stylesheet" href="css/style.min.css"/>
+	</head>
+	<body>
+	<?php
+		include(__DIR__.'/templates/header.html');
+	?>
+	</body>
+</html>
