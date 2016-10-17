91f3d7e7673a6e09d557ee0471795527f8f9f05d
pavan_08 <pavanchhatpar@gmail.com>
2016-10-17 18:39:01 +0530

    sign up form php ready, favico icon added

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 	<head>
 		<title>Virtual Stock Market!</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="images/favico.png">
+		<link rel="apple-touch-icon" type="image/png" href="images/favico.png">
 		<link rel="stylesheet" href="css/style.min.css"/>
 		<script type="text/javascript" src="js/minified/jquery-3.1.1.min.js"></script>
 
 	</head>
 	<body>
-	<div class="home-content">
 	<?php
+		include(__DIR__.'/classes/SignUp.php');
 		include(__DIR__.'/views/home.php');
 	?>
-	</div>
 	</body>
 </html>
